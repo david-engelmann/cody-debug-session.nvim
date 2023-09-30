7c026f24db9289d4e5fb0637949aa897b54ff54b
David Engelmann <david+4113@satisfilabs.com>
2023-09-30 17:26:31 -0400

    got BufEnter to work

--- lua/cody-debug-session.lua
+++ lua/cody-debug-session.lua
@@ -7,14 +7,16 @@ local enabled = false
 M.enable = function()
   enabled = true
   vim.api.nvim_clear_autocmds { group = group }
-  print ("help on the way")
---  vim.api.nvim_create_autocmd("EnterDebugSession", {
---    group = group,
---    callback = function()
---      if enabled then
---        print ("hello")
---      end
---  end})
+  
+--  print ("help on the way")
+  vim.api.nvim_create_autocmd("BufEnter", {
+    group = group,
+    callback = function()
+      if enabled then
+        print("hello")
+      end
+    end,
+  })
 end
 
 M.disable = function()
